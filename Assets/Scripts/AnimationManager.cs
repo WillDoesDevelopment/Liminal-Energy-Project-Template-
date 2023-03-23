040b9ea12570013d54af238bac275d186434c69b
WillDoesDevelopment <s3901070@student.rmit.edu.au>
2023-03-24 08:44:53 +1100

    Altered the project
    The music values now work fine its just very all over the place, now all i need to do is use a Fourier transform on my audio. I got rid of the scrolling mesh and replaced it with one long mesh with scrolling noise

---
+++ Assets/Scripts/AnimationManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationManager : MonoBehaviour
+{
+    [SerializeField] public Animator SunRiseAnim;
+    public GameObject SunObj;
+    private int gameDuration = 170;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        SunSpinInput();
+    }
+
+
+
+    public void SunSpinInput()
+    {
+        SunObj.transform.position += new Vector3(0, Time.time / 135/170);
+        SunObj.transform.GetChild(0).transform.eulerAngles += new Vector3(0,50*Time.deltaTime,0);
+        SunObj.transform.GetChild(1).transform.eulerAngles += new Vector3(0,50*Time.deltaTime,0);
+    }
+}
---
+++ Assets/Scripts/ValleyManager.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ValleyManager : MonoBehaviour
+{
+   
+    public AudioSource backGroundMusic;
+
+    public CustomMesh CM;
+    public GameObject Camera;
+    public int ValleyAmount;
+    public GameObject CustomMeshObj;
+    public GameObject[] ValleyObjs;
+
+    public GameObject SunObj;
+
+    public Material Mat;    
+
+    public float gapAdjustment;
+    // Start is called before the first frame update
+
+
+
+    void Start()
+    {
+
+
+
+        //Camera.transform.position = new Vector3(CM.MeshGridSize_x / 2,10, 150);
+        //customMesh.MeshGridLength;
+        ValleyObjs = new GameObject[ValleyAmount];
+        //SunObj.transform.position = new Vector3(CM.MeshGridSize_x / 2-7, -45, ValleyObjs.Length * CM.MeshGridLength );
+        /*for (int i =0; i<ValleyObjs.Length; i++)
+        {
+            ValleyObjs[i] = Instantiate(CustomMeshObj, new Vector3(0, 0, i*(CM.MeshGridLength)- 1), Quaternion.identity);
+            ValleyObjs[i].transform.localEulerAngles = new Vector3(-0.01f, 0, 0);
+            *//*if(i%2 == 0)
+            {
+                ValleyObjs[i].transform.localScale = new Vector3(1, 1, -1f);
+                ValleyObjs[i].transform.position += new Vector3(0,0,CM.MeshGridLength);
+            }*//*
+
+        }*/
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+       /* for (int i = 0; i < ValleyObjs.Length; i++)
+        {
+            //ValleyObjs[i].transform.position += new Vector3(0f, 0f, -10f * Time.deltaTime);
+
+            if (ValleyObjs[i].transform.position.z < 0f )
+            {
+                //ValleyObjs[i].SetActive(false);
+                ValleyObjs[i].transform.position = new Vector3(0,0,(CM.MeshGridLength-0.005f) *(ValleyAmount-1));
+            }
+        }*/
+    }
+
+
+}
